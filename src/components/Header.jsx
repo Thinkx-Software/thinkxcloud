550d0fd96661e7a4c81c6edb56a3cb5aeed51deb
Ronald Ngarombo <ronaldngzgadro@gmail.com>
2021-10-05 22:02:05 +0300

    Fix 404 routes in production
    Defaulted to the default "Link" from react-router-dom as it should have been in the first place.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,128 +7,115 @@ import NavbarToggler from '@material-tailwind/react/NavbarToggler';
 import NavbarCollapse from '@material-tailwind/react/NavbarCollapse';
 import Nav from '@material-tailwind/react/Nav';
 import NavLink from '@material-tailwind/react/NavLink';
-import Image from "@material-tailwind/react/Image";
+import Image from '@material-tailwind/react/Image';
 
+import { Link } from 'react-router-dom';
 
 export default function DefaultNavbar() {
     const [openNavbar, setOpenNavbar] = useState(false);
 
     return (
         <div className=" shadow-md ">
-             <Navbar color="blueGray" navbar>
-            <NavbarContainer>
-                <NavbarWrapper>
-                    <a
-                        href="https://www.thinkxsoftware.com"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <div  className="flex flex-row items-center justify-between">
-                        <Image
-            src="/assets/logo2.jpeg"
-            className="h-12 w-12 mr-2"
-            rounded={true}
-            raised={true}
-            alt="logo"
-        />
-                        
-                        <NavbarBrand>Thinkx Cloud</NavbarBrand>
+            <Navbar color="blueGray" navbar>
+                <NavbarContainer>
+                    <NavbarWrapper>
+                        <a
+                            href="https://www.thinkxsoftware.com"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <div className="flex flex-row items-center justify-between">
+                                <Image
+                                    src="/assets/logo2.jpeg"
+                                    className="h-12 w-12 mr-2"
+                                    rounded={true}
+                                    raised={true}
+                                    alt="logo"
+                                />
 
-                        </div>
+                                <NavbarBrand>Thinkx Cloud</NavbarBrand>
+                            </div>
+                        </a>
+                        <NavbarToggler
+                            onClick={() => setOpenNavbar(!openNavbar)}
+                            color="white"
+                        />
+                    </NavbarWrapper>
 
-                    </a>
-                    <NavbarToggler
-                        onClick={() => setOpenNavbar(!openNavbar)}
-                        color="white"
-                    />
-                </NavbarWrapper>
+                    <NavbarCollapse open={openNavbar}>
+                        <Nav>
+                            <div className="flex flex-col z-50 lg:flex-row lg:items-center">
+                                <Link
+                                    to="/"
+                                    rel="noreferrer"
+                                    className="text-white px-3"
+                                >
+                                    Home
+                                </Link>
 
-                <NavbarCollapse open={openNavbar}>
-                    <Nav>
-                        <div className="flex flex-col z-50 lg:flex-row lg:items-center">
-                        <NavLink
-                                href="/"
+                                <Link
+                                    to="/sharedhosting"
+                                    rel="noreferrer"
+                                    className="text-white px-3"
+                                >
+                                    Shared Hosting
+                                </Link>
 
-                                rel="noreferrer"
-                                ripple="light"
-                            >
-                           Home
-                                
-                            </NavLink>
-                            <NavLink
-                                href="/sharedhosting"
-                                
-                                rel="noreferrer"
-                                ripple="light"
-                            >
-                           Shared Hosting
-                                
-                            </NavLink>
-                            <NavLink
-                                href="/vpshosting"
-                            
-                                rel="noreferrer"
-                                ripple="light"
-                            >
-                                VPS Hosting
-                            </NavLink>
-                            <NavLink
-                                href="/ssl"
-                            
-                                rel="noreferrer"
-                                ripple="light"
-                            >
-                                SSL
-                            </NavLink>
-                            <NavLink
-                                href="https://thinkxcloud.com/billing/cart.php?a=add&domain=register"
-                                target="_blank"
-                                rel="noreferrer"
-                                ripple="light"
-                            >
-                                Domain
-                            </NavLink>
-                            
+                                <Link
+                                    to="/vpshosting"
+                                    rel="noreferrer"
+                                    className="text-white px-3"
+                                >
+                                    VPS Hosting
+                                </Link>
 
-                            <NavLink
-                                href="https://thinkxcloud.com/billing/index.php?rp=/login"
-                                target="_blank"
-                                rel="noreferrer"
-                                ripple="light"
-                            >
-                                <h2 className=" md:mr-5 rounded-full   p-2 cursor-pointer text-center w-24 text-gray-900 bg-gray-200
-                               hover:bg-blue-700 hover:text-white
-                    
-                              ">
-                                 LOGIN
-                              </h2>
-                            </NavLink>
+                                <Link
+                                    to="/ssl"
+                                    rel="noreferrer"
+                                    className="text-white px-3"
+                                >
+                                    SSL
+                                </Link>
 
-                            <NavLink
-                                href="https://thinkxcloud.com/billing/register.php"
-                                target="_blank"
-                                rel="noreferrer"
-                                ripple="light"
-                            >
-                                <h2 className=" md:mr-5 rounded-full   p-2 cursor-pointer text-center w-24 text-gray-900 bg-gray-200
-                               hover:bg-blue-700 hover:text-white
-                    
-                              ">
-                                 SIGN UP
-                              </h2>
-                            </NavLink>
+                                <Link
+                                    to="/billing/cart.php?a=add&domain=register"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="text-white px-3"
+                                >
+                                    Domain
+                                </Link>
 
+                                <Link
+                                    to="https://thinkxcloud.com/billing/index.php?rp=/login"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <h2
+                                        className=" md:mr-5 rounded-full   p-2 cursor-pointer text-center w-24 text-gray-900 bg-gray-200
+                               hover:bg-blue-700 hover:text-white"
+                                    >
+                                        LOGIN
+                                    </h2>
+                                </Link>
 
-                            
-                        </div>
-                    </Nav>
-                </NavbarCollapse>
-            </NavbarContainer>
-        </Navbar>
-
-
+                                <Link
+                                    to="https://thinkxcloud.com/billing/register.php"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <h2
+                                        className=" md:mr-5 rounded-full   p-2 cursor-pointer text-center w-24 text-gray-900 bg-gray-200
+                               hover:bg-blue-700 hover:text-white"
+                                    >
+                                        SIGN UP
+                                    </h2>
+                                </Link>
+                            </div>
+                        </Nav>
+                    </NavbarCollapse>
+                </NavbarContainer>
+            </Navbar>
         </div>
-
     );
 }
-
--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,43 +1,43 @@
-import React from 'react'
+import React from 'react';
 import Header from '../components/Header';
-import {BrowserRouter, Switch, Route} from  "react-router-dom";
-import HomePage from "../pages/HomePage/HomePage"
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage/HomePage';
 import Footer from '../components/Footer';
 import SharedHosting from '../pages/HomePage/SharedHosting';
 import VPSHosting from '../pages/VPSHosting';
 import SSL from '../pages/SSL';
-import NotFound from "../pages/NotFound"
-
+import NotFound from '../pages/NotFound';
 
 function Routing() {
     return (
         <div>
             <BrowserRouter>
-            <Header/>
-            <Switch>
-                <Route path="/sharedhosting" exact>
-                    <SharedHosting/>
-                </Route>
-                <Route path="/vpshosting">
-                  <VPSHosting/>
-                </Route>
-                <Route path="/ssl" exact>
-                    <SSL/>
-                </Route>
+                <Header />
+                <Switch>
+                    <Route path="/" exact>
+                        <HomePage />
+                    </Route>
 
-                <Route path="/" exact>
-                    <HomePage/>
-                </Route>
-                <Route path="*">
-                   <NotFound/>
-               </Route>
+                    <Route path="/sharedhosting">
+                        <SharedHosting />
+                    </Route>
 
-            </Switch>
-            <Footer/>
+                    <Route path="/vpshosting">
+                        <VPSHosting />
+                    </Route>
+
+                    <Route path="/ssl">
+                        <SSL />
+                    </Route>
+
+                    <Route path="*">
+                        <NotFound />
+                    </Route>
+                </Switch>
+                <Footer />
             </BrowserRouter>
-            
         </div>
-    )
+    );
 }
 
-export default Routing
+export default Routing;
